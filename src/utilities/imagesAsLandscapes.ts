dd22c40dbb8088b41c53fef47f2f657fd354fe32
mikecebul <mikecebul@gmail.com>
2024-09-25 16:39:31 -0400

    Fix: rename AboutUsBlock type

--- src/utilities/imagesAsLandscapes.ts
+++ src/utilities/imagesAsLandscapes.ts
@@ -1,11 +1,11 @@
-import type { AboutUsBlock, Landscape } from '@/payload-types'
+import type { Landscape, RichTextBlock } from '@/payload-types'
 
 export type SanitizedImage = {
   image: Landscape
   id: string
 }
 
-export const imagesAsLandscapes = (images: AboutUsBlock['images']): Landscape[] => {
+export const imagesAsLandscapes = (images: RichTextBlock['images']): Landscape[] => {
   const isLandscape = (item: any): item is Landscape => {
     return typeof item === 'object' && item !== null && 'url' in item
   }
