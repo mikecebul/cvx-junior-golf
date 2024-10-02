1d9e275d63b04937c77e994ed384a3abdbc9b101
mikecebul <mikecebul@gmail.com>
2024-10-01 23:58:50 -0400

    Test: See if webhook  is working

--- src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
+++ src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
@@ -1,29 +1,23 @@
+import payloadConfig from '@payload-config'
 import type { StripeWebhookHandler } from '@payloadcms/plugin-stripe/types'
+import { getPayload } from 'payload'
 import type Stripe from 'stripe'
 
 export const checkoutSessionCompleted: StripeWebhookHandler<{
   data: {
     object: Stripe.Checkout.Session
   }
-}> = async ({ event, payload }) => {
-  payload.logger.info('checkoutSessionCompleted handler called')
+}> = async ({ event }) => {
+  const payload = await getPayload({ config: payloadConfig })
 
   try {
-    payload.logger.info('About to destructure event.data.object')
     const { id: sessionId, metadata, amount_total } = event.data.object
-    payload.logger.info('Destructuring successful')
-
-    payload.logger.info('Checking submissionId')
     const submissionId = metadata?.submissionId
 
     if (!submissionId) {
-      payload.logger.info('No submissionId found')
-      payload.logger.error('No submissionId found in checkout session metadata')
+      payload.logger.error('No submissionId found')
       return
     }
-
-    payload.logger.info(`submissionId found: ${submissionId}`)
-    payload.logger.info('Try catch initiated')
 
     const numericSubmissionId = parseInt(submissionId, 10)
 
@@ -31,7 +25,7 @@ export const checkoutSessionCompleted: StripeWebhookHandler<{
       throw new Error('Invalid submissionId: not a number')
     }
 
-    const updateResult = await payload.update({
+    await payload.update({
       collection: 'form-submissions',
       id: numericSubmissionId, // Use the converted numeric ID
       overrideAccess: true,
@@ -41,14 +35,10 @@ export const checkoutSessionCompleted: StripeWebhookHandler<{
       },
     })
 
-    payload.logger.info('Payload form update result:', updateResult)
-
     payload.logger.info(
       `Successfully updated form submission ${numericSubmissionId} for checkout session ${sessionId}`,
     )
   } catch (error) {
-    payload.logger.error('Error in checkoutSessionCompleted:', error)
     payload.logger.error(`Error updating form submission: ${error}`)
-    payload.logger.error('Error stack:', error.stack)
   }
 }
