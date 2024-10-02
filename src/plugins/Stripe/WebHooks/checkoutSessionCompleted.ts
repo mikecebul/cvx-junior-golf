a4cbb943b691939a2ab9c4859566d4e737f65f6d
mikecebul <mikecebul@gmail.com>
2024-10-02 19:26:17 -0400

    Test: See if webhook is working without try catch

--- src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
+++ src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
@@ -6,40 +6,37 @@ export const checkoutSessionCompleted: StripeWebhookHandler<{
     object: Stripe.Checkout.Session
   }
 }> = async ({ event, payload }) => {
+  const { id: sessionId, metadata, amount_total } = event.data.object
+  const submissionId = metadata?.submissionId
 
-  try {
-    const { id: sessionId, metadata, amount_total } = event.data.object
-    const submissionId = metadata?.submissionId
-
-    if (!submissionId) {
-      payload.logger.error('No submissionId found')
-      return
-    }
-
-    const submission = await payload.findByID({
-      collection: 'form-submissions',
-      id: submissionId,
-    })
-    if (!submission) {
-      payload.logger.error(`No submission found for id ${submissionId}`)
-      return
-    }
-
-    const updatedSubmission = await payload.update({
-      collection: 'form-submissions',
-      id: submissionId,
-      data: {
-        status: 'paid',
-        amount: `$${(amount_total ?? 0) / 100}`,
-      },
-    })
-    if (!updatedSubmission) {
-      payload.logger.error(`Error updating form submission ${submissionId} for checkout session ${sessionId}`)
-      return
-    }
-
-    payload.logger.info(`Updated form submission:, ${updatedSubmission}`)
-  } catch (error) {
-    payload.logger.error(`Error updating form submission: ${error}`)
+  if (!submissionId) {
+    payload.logger.error('No submissionId found')
+    return
   }
+
+  const submission = await payload.findByID({
+    collection: 'form-submissions',
+    id: submissionId,
+  })
+  if (!submission) {
+    payload.logger.error(`No submission found for id ${submissionId}`)
+    return
+  }
+
+  const updatedSubmission = await payload.update({
+    collection: 'form-submissions',
+    id: submissionId,
+    data: {
+      status: 'paid',
+      amount: `$${(amount_total ?? 0) / 100}`,
+    },
+  })
+  if (!updatedSubmission) {
+    payload.logger.error(
+      `Error updating form submission ${submissionId} for checkout session ${sessionId}`,
+    )
+    return
+  }
+
+  payload.logger.info(`Updated form submission:, ${updatedSubmission}`)
 }
