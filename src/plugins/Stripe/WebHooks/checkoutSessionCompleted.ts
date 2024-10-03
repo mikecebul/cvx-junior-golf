74da69e6ba06e2b06ecda787ee91d3b35d0f1bec
mikecebul <mikecebul@gmail.com>
2024-10-03 12:45:45 -0400

    simplify Webhook test find w/ override access

--- src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
+++ src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
@@ -10,34 +10,14 @@ export const checkoutSessionCompleted: StripeWebhookHandler<{
 }> = async ({ event, payload }) => {
   payload.logger.info(`Webhook ran: ${JSON.stringify(event)}`)
   try {
-    // const { metadata, amount_total } = event.data.object
-    // const submissionId = metadata?.submissionId
-
-    // if (!submissionId) {
-    //   payload.logger.error('No submissionId found')
-    //   return
-    // }
-
-    // const updatedSubmission = await payload.update({
-    //   collection: 'form-submissions',
-    //   id: submissionId,
-    //   data: {
-    //     status: 'paid',
-    //     // amount: ((amount_total ?? 0) / 100).toString(),
-    //   },
-    // })
     const testFind = await payload.find({
       collection: 'events',
       depth: 1,
-      limit: 1,
-      page: 1,
-      sort: 'createdAt',
       overrideAccess: true,
     })
 
-    // payload.logger.info(`Updated form submission: ${JSON.stringify(updatedSubmission)}`)
     payload.logger.info(`Payload find ran: ${JSON.stringify(testFind)}`)
   } catch (error) {
-    payload.logger.error(`Webhook Error updating form submission: ${error}`)
+    payload.logger.error(`Webhook Error using localAPI: ${error}`)
   }
 }
