1f90b59083a0508b8816f72cabecd3adb095de96
mikecebul <mikecebul@gmail.com>
2024-10-03 09:15:07 -0400

    test webook to update form with just status

--- src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
+++ src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
@@ -1,3 +1,5 @@
+'use server'
+
 import type { StripeWebhookHandler } from '@payloadcms/plugin-stripe/types'
 import type Stripe from 'stripe'
 
@@ -7,36 +9,25 @@ export const checkoutSessionCompleted: StripeWebhookHandler<{
   }
 }> = async ({ event, payload }) => {
   try {
-    const { id: sessionId, metadata, amount_total } = event.data.object
+    const { metadata, amount_total } = event.data.object
     const submissionId = metadata?.submissionId
 
     if (!submissionId) {
       payload.logger.error('No submissionId found')
       return
     }
-    const numericSubmissionId = parseInt(submissionId, 10)
-
-    if (isNaN(numericSubmissionId)) {
-      throw new Error('Invalid submissionId: not a number')
-    }
 
     const updatedSubmission = await payload.update({
       collection: 'form-submissions',
-      id: numericSubmissionId,
+      id: submissionId,
       data: {
         status: 'paid',
-        amount: `$${(amount_total ?? 0) / 100}`,
+        // amount: ((amount_total ?? 0) / 100).toString(),
       },
     })
-    if (!updatedSubmission) {
-      payload.logger.error(
-        `Error updating form submission ${submissionId} for checkout session ${sessionId}`,
-      )
-      return
-    }
 
-    payload.logger.info(`Updated form submission:, ${updatedSubmission}`)
+    payload.logger.info(`Updated form submission: ${JSON.stringify(updatedSubmission)}`)
   } catch (error) {
-    payload.logger.error(`Error updating form submission: ${error}`)
+    payload.logger.error(`Webhook Error updating form submission: ${error}`)
   }
 }
