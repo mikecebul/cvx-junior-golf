d1e5838c1f0cada1397650c123cfad1deaaddb22
mikecebul <mikecebul@gmail.com>
2024-10-03 12:22:57 -0400

    Webhook test find

--- src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
+++ src/plugins/Stripe/WebHooks/checkoutSessionCompleted.ts
@@ -8,25 +8,34 @@ export const checkoutSessionCompleted: StripeWebhookHandler<{
     object: Stripe.Checkout.Session
   }
 }> = async ({ event, payload }) => {
+  payload.logger.info(`Webhook ran: ${JSON.stringify(event)}`)
   try {
-    const { metadata, amount_total } = event.data.object
-    const submissionId = metadata?.submissionId
+    // const { metadata, amount_total } = event.data.object
+    // const submissionId = metadata?.submissionId
 
-    if (!submissionId) {
-      payload.logger.error('No submissionId found')
-      return
-    }
+    // if (!submissionId) {
+    //   payload.logger.error('No submissionId found')
+    //   return
+    // }
 
-    const updatedSubmission = await payload.update({
-      collection: 'form-submissions',
-      id: submissionId,
-      data: {
-        status: 'paid',
-        // amount: ((amount_total ?? 0) / 100).toString(),
-      },
+    // const updatedSubmission = await payload.update({
+    //   collection: 'form-submissions',
+    //   id: submissionId,
+    //   data: {
+    //     status: 'paid',
+    //     // amount: ((amount_total ?? 0) / 100).toString(),
+    //   },
+    // })
+    const testFind = await payload.find({
+      collection: 'events',
+      depth: 1,
+      limit: 1,
+      page: 1,
+      sort: 'createdAt',
     })
 
-    payload.logger.info(`Updated form submission: ${JSON.stringify(updatedSubmission)}`)
+    // payload.logger.info(`Updated form submission: ${JSON.stringify(updatedSubmission)}`)
+    payload.logger.info(`Payload find ran: ${JSON.stringify(testFind)}`)
   } catch (error) {
     payload.logger.error(`Webhook Error updating form submission: ${error}`)
   }
