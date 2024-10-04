fcc50a9e2d2f2ee2dd6c7ef56980de704d3fcf8b
mikecebul <mikecebul@gmail.com>
2024-10-04 14:36:04 -0400

    Fix(access): fiix access type errors

--- src/access/checkRole.ts
+++ src/access/checkRole.ts
@@ -1,6 +1,6 @@
 import type { User } from '@/payload-types'
 
-export const checkRole = (role: string, user: User | null): boolean => {
-  if (!user) return false
-  return user.role === role
+export const checkRole = (role: string, user: unknown): boolean => {
+  if (!user || typeof user !== 'object' || !('role' in user)) return false
+  return (user as User).role === role
 }
--- src/collections/Events/index.ts
+++ src/collections/Events/index.ts
@@ -1,5 +1,6 @@
 import { anyone } from '@/access/anyone'
 import { authenticated } from '@/access/authenticated'
+import { superAdmin } from '@/access/superAdmin'
 import { CollectionConfig } from 'payload'
 
 export const Events: CollectionConfig = {
@@ -19,6 +20,7 @@ export const Events: CollectionConfig = {
   },
   admin: {
     useAsTitle: 'title',
+    hideAPIURL: !superAdmin,
   },
   fields: [
     {
