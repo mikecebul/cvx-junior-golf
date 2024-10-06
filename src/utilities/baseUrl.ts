c59bfda1610c3a2b9567b2268c4252343c173e6b
mikecebul <mikecebul@gmail.com>
2024-10-05 22:46:07 -0400

    Fix: vercel url typo

--- src/utilities/baseUrl.ts
+++ src/utilities/baseUrl.ts
@@ -2,5 +2,5 @@ export const baseUrl =
   process.env.VERCEL_ENV === 'production'
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
     : process.env.VERCEL_ENV === 'preview'
-      ? `https://${process.env.VERCEL_BRANCH_URL}.vercel.app`
+      ? `https://${process.env.VERCEL_BRANCH_URL}`
       : `http://localhost:3000`
