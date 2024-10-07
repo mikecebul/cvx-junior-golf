ea055bd8b5ac99ca0d9b7a73fefc3b21012a31b3
mikecebul <mikecebul@gmail.com>
2024-10-07 09:48:55 -0400

    Chore: more balanced logo

--- src/components/Logo/Graphic.tsx
+++ src/components/Logo/Graphic.tsx
@@ -2,8 +2,8 @@ import { LandPlot } from 'lucide-react'
 
 const Graphic = () => {
   return (
-    <div className="flex justify-center items-end w-full">
-      <LandPlot className="size-12 text-primary" />
+    <div className="flex justify-center items-center w-72">
+      <LandPlot strokeWidth={2.2} className="size-16 text-primary shrink-0" />
       <span className="ml-4 text-4xl font-extrabold text-primary flex-wrap md:flex-nowrap">
         Charlevoix Junior Golf
       </span>
