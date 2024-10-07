5a69787a2e223c1ac908aba816d1de11afb23bbc
mikecebul <mikecebul@gmail.com>
2024-10-07 11:01:38 -0400

    Chore: even prettier logo on admin login

--- src/components/Logo/Graphic.tsx
+++ src/components/Logo/Graphic.tsx
@@ -2,11 +2,11 @@ import { LandPlot } from 'lucide-react'
 
 const Graphic = () => {
   return (
-    <div className="flex justify-center items-center w-72">
-      <LandPlot strokeWidth={2.2} className="size-16 text-primary shrink-0" />
-      <span className="ml-4 text-4xl font-extrabold text-primary flex-wrap md:flex-nowrap">
+    <div className="flex justify-center items-center w-full mx-auto">
+      <LandPlot className="size-28 text-primary shrink-0" />
+      <h1 className="ml-4 text-6xl font-bold text-primary text-balance max-w-80">
         Charlevoix Junior Golf
-      </span>
+      </h1>
     </div>
   )
 }
