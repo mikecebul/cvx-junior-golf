d3bcbec301cacbc5291bd8fa83b5d465aec3065c
mikecebul <mikecebul@gmail.com>
2024-10-16 20:57:59 -0400

    upgrade beta.115, drizzle, libsql, autosave fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241006_054028 from './20241006_054028';
+import * as migration_20241017_005657 from './20241017_005657';
 
 export const migrations = [
   {
     up: migration_20241006_054028.up,
     down: migration_20241006_054028.down,
-    name: '20241006_054028'
+    name: '20241006_054028',
+  },
+  {
+    up: migration_20241017_005657.up,
+    down: migration_20241017_005657.down,
+    name: '20241017_005657'
   },
 ];
