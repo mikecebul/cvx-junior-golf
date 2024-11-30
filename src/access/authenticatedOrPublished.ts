b9d9d0e247c581d87ea3eea033c99480ecb0260c
mikecebul <mikecebul@gmail.com>
2024-11-30 16:24:44 -0500

    Should not of changed this access control file

--- src/access/authenticatedOrPublished.ts
+++ src/access/authenticatedOrPublished.ts
@@ -1,8 +1,8 @@
 import type { Access } from 'payload'
 
 export const authenticatedOrPublished: Access = ({ req: { user } }) => {
-  if (!user) {
-    return false
+  if (user) {
+    return true
   }
 
   return {
