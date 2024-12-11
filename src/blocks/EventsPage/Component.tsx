f49892e4162e4f271c91c4cc9ffd158f98738182
mikecebul <mikecebul@gmail.com>
2024-12-11 17:36:17 -0500

    update typography on eventsPage

--- src/blocks/EventsPage/Component.tsx
+++ src/blocks/EventsPage/Component.tsx
@@ -4,12 +4,13 @@ import { EventsPageBlock as EventsPageBlockType } from '@/payload-types'
 import { CalendarIcon, MapPinIcon } from 'lucide-react'
 import { format } from 'date-fns'
 import RichText from '@/components/RichText'
+import { Title } from '@/components/Hero/HeroMedium'
 
 export const EventsPageBlock = ({ title, eventCards, announcements }: EventsPageBlockType) => {
   return (
     <Container className="space-y-16">
       <div className="space-y-8">
-        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <Title text={title ?? 'Upcoming Events'} heading="h1" />
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
           {Array.isArray(eventCards) &&
             eventCards.length > 0 &&
@@ -46,7 +47,7 @@ export const EventsPageBlock = ({ title, eventCards, announcements }: EventsPage
       </div>
       {announcements && announcements.length > 0 && (
         <div className="space-y-8">
-          <h2 className="text-2xl font-bold mb-4">Announcements</h2>
+          <Title text="Announcements" heading="h2" />
           <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
             {announcements?.map((announcement) => {
               if (typeof announcement !== 'object') return null
