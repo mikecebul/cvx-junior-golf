5f76725e1d227226a8d0e3edbed890a45fa0eda0
mikecebul <mikecebul@gmail.com>
2024-12-13 23:03:28 -0500

    Update sitemap

--- src/collections/Pages/hooks/revalidateDelete.ts
+++ src/collections/Pages/hooks/revalidateDelete.ts
@@ -1,12 +1,12 @@
-import { Page } from "@/payload-types"
-import { revalidatePath, revalidateTag } from "next/cache"
-import { CollectionAfterDeleteHook } from "payload"
+import { Page } from '@/payload-types'
+import { revalidatePath, revalidateTag } from 'next/cache'
+import { CollectionAfterDeleteHook } from 'payload'
 
 export const revalidateDelete: CollectionAfterDeleteHook<Page> = ({ doc, req: { context } }) => {
   if (!context.disableRevalidate) {
     const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
     revalidatePath(path)
-    revalidateTag('pages-sitemap')
+    revalidateTag('sitemap')
   }
 
   return doc
--- src/collections/Pages/hooks/revalidatePage.ts
+++ src/collections/Pages/hooks/revalidatePage.ts
@@ -1,27 +1,29 @@
 import type { CollectionAfterChangeHook } from 'payload'
 
-import { revalidatePath } from 'next/cache'
+import { revalidatePath, revalidateTag } from 'next/cache'
 
 import type { Page } from '../../../payload-types'
 
-export const revalidatePage: CollectionAfterChangeHook<Page> = ({
-  doc,
-  previousDoc,
-  req,
-}) => {
+export const revalidatePage: CollectionAfterChangeHook<Page> = ({ doc, previousDoc, req }) => {
   if (doc._status === 'published') {
     const path = doc.slug === 'home' ? '/' : `/${doc.slug}`
 
-    req.headers['X-Payload-Migration'] !== 'true' && revalidatePath(path)
-    req.payload.logger.info(`Revalidating page at path: ${path}`)
+    if (req.headers['X-Payload-Migration'] !== 'true') {
+      req.payload.logger.info(`Revalidating page at path: ${path}`)
+      revalidatePath(path)
+      revalidateTag('sitemap')
+    }
   }
 
   // If the page was previously published, we need to revalidate the old path
   if (previousDoc?._status === 'published' && doc._status !== 'published') {
     const oldPath = previousDoc.slug === 'home' ? '/' : `/${previousDoc.slug}`
 
-    req.headers['X-Payload-Migration'] !== 'true' && revalidatePath(oldPath)
-    req.payload.logger.info(`Revalidating old page at path: ${oldPath}`)
+    if (req.headers['X-Payload-Migration'] !== 'true') {
+      req.payload.logger.info(`Revalidating old page at path: ${oldPath}`)
+      revalidatePath(oldPath)
+      revalidateTag('sitemap')
+    }
   }
 
   return doc
