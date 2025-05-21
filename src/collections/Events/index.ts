898802b11294cc82b91d443f1bafa18cb5698813
mikecebul <mikecebul@gmail.com>
2025-05-21 10:24:36 -0400

    feat: add afterChange hook to revalidate paths on event updates

--- src/collections/Events/index.ts
+++ src/collections/Events/index.ts
@@ -2,6 +2,7 @@ import { authenticatedOrPublished } from '@/access/authenticatedOrPublished'
 import { editorOrHigher } from '@/access/editorOrHigher'
 import { superAdmin } from '@/access/superAdmin'
 import { HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
+import { revalidatePath } from 'next/cache'
 import { CollectionConfig } from 'payload'
 
 export const Events: CollectionConfig = {
@@ -52,4 +53,14 @@ export const Events: CollectionConfig = {
       required: true,
     },
   ],
+  hooks: {
+    afterChange: [
+      ({ req }) => {
+        if (req.headers['X-Payload-Migration'] !== 'true') {
+          revalidatePath('/(payload)', 'layout')
+          revalidatePath('/(frontend)', 'layout')
+        }
+      },
+    ],
+  },
 }
